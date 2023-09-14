306f62a0a0bb10cd554217c6e738f0613573c1d5
Norbi1027 <sandornorbi7@gmail.com>
2023-09-14 13:03:59 +0200

    Add project files.

---
+++ login/Connect.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows;
+
+namespace login
+{
+    internal class Connect
+    {
+        private MySqlConnection conn;
+        private string server;
+        private string user;
+        private string pass;
+        private string db;
+
+        public Connect()
+        { 
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            server = "localhost";
+            db = "pharmacy";
+            user = "root";
+            pass = "";
+            string connectionSring;
+            connectionSring = "Data Source=" + server + ";Database=" + db + ";User Id" + user + ";password=" + pass + ";SSL Mode=0";
+            conn = new MySqlConnection(connectionSring);
+            
+        }
+        public bool OpenConnection()
+        {
+            try
+            {
+                conn.Open();
+                return true;
+            }
+            catch (MySqlException e)
+            {
+
+                switch(e.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Nem lehet csatlakozni a szerverhez.");
+                        break;
+                        case 1045:
+                        MessageBox.Show("Rossz felhasználónév vagy jelszó,próbáld újra.");
+                        break;
+                }
+                return false;
+            }
+        }
+        public void close_conn()
+        {
+            this.conn.Close();
+        }
+        public MySqlConnection get_connection()
+        {
+            return this.conn;
+        }
+    }
+}
---
+++ login/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace login
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string username,password, sql;
+        private Connect conn = new Connect();
+        private MySqlCommand command;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+       private void login(object sender, RoutedEventArgs e)
+        {
+            username = user.Text;
+            password = pass.Password;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) )
+            {
+                MessageBox.Show("Töltsd ki a mezőt!");
+            }
+            else
+            {
+                sql = "SELECT username, password FROM user WHERE username = '" + username + "' AND password = '" + password + "'";
+                if (conn.OpenConnection() == true)
+                {
+                    try
+                    {
+                        command = new MySqlCommand(sql, conn.get_connection());
+                        object a = command.ExecuteScalar();
+                        if (a == null)
+                        {
+                            MessageBox.Show("Rossz felhasználónév vagy jelszó.");
+                        }
+                        else
+                        {
+                            Dashboard dsb = new Dashboard();
+                            dsb.Show();
+                            this.Close();
+                        }
+                    }
+                    catch (MySqlException x)
+                    {
+
+                        MessageBox.Show("" + x);
+                    }
+                }
+            }
+            user.Text = "";
+            pass.Password = "";
+            conn.close_conn();
+        }
+    }
+}
